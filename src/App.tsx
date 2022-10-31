048a9fadccdd770a9d3369d017faa3a5ec0c126b
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-10-31 11:11:02 +0900

    hover, click movement

--- src/App.tsx
+++ src/App.tsx
@@ -13,61 +13,20 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: white;
   border-radius: 30px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 2px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: #fff;
-  width: 70px;
-  height: 70px;
-  border-radius: 35px;
-  place-self: center;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 2px rgba(0, 0, 0, 0.06);
-`;
-
 const boxVar = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delay: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const circleVar = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
-  },
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px" },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVar} initial="start" animate="end">
-        <Circle variants={circleVar} />
-        <Circle variants={circleVar} />
-        <Circle variants={circleVar} />
-        <Circle variants={circleVar} />
-      </Box>
+      <Box variants={boxVar} whileHover="hover" whileTap="click" />
     </Wrapper>
   );
 }
